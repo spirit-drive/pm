053092bbea5be9888f2670dff129b4255352bb67
Igor Zvyagin <spirit-drive@yandex.ru>
2023-08-16 09:35:09 +0300

    remove ads and contacts

--- src/components/ScreenBottom/ScreenBottom.tsx
+++ src/components/ScreenBottom/ScreenBottom.tsx
@@ -8,12 +8,6 @@ export type ScreenBottomProps = {
 
 export const ScreenBottom = memo<ScreenBottomProps>(({ className }) => (
   <div className={cn(s.root, className)}>
-    <div className={s.block}>
-      <div>Приложение работает на платной основе, потому буду благодарен за поддержку</div>
-      <div>Тинькофф: 5536 9137 9113 9112</div>
-      <div>Яндекс кошелек: 410012281752876</div>
-      <div>Буду рад обратной связи в телеграмм @spirit_drive</div>
-    </div>
     Icons made by{' '}
     <a href="https://smashicons.com/" title="Smashicons">
       Smashicons
